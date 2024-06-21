1d4cfaadeb53ab80104703facb8005d615fb433c
Victor Hugo de Lima Kunst <victor.kunst@serpro.gov.br>
2024-06-21 09:42:14 -0300

    Echo

--- centralize.sh
+++ centralize.sh
@@ -22,14 +22,14 @@ CUR_WINDOW_GEOM=$(xdotool getwindowgeometry $CUR_WINDOW_PID | grep -o '[0-9]*x[0
 CUR_WINDOW_GEOM_H=$(cut -d "x" -f2 <<< $CUR_WINDOW_GEOM)
 CUR_WINDOW_GEOM_W=$(cut -d "x" -f1 <<< $CUR_WINDOW_GEOM)
 
-echo $CUR_WINDOW_GEOM_W "x" $CUR_WINDOW_GEOM_H
+echo "Window size: " $CUR_WINDOW_GEOM_W "x" $CUR_WINDOW_GEOM_H
 
 # get current screen info
 WIN_DESKTOP_DIMS=$(xdpyinfo | grep dimensions | grep -o '[0-9x]*' | head -n1)
 WIN_DESTOP_H=$(cut -d "x" -f2 <<< $WIN_DESKTOP_DIMS)
 WIN_DESTOP_W=$(cut -d "x" -f1 <<< $WIN_DESKTOP_DIMS)
 
-echo $WIN_DESTOP_W "x" $WIN_DESTOP_H
+echo "Screen size: " $WIN_DESTOP_W "x" $WIN_DESTOP_H
 
 # get middle of the screen
 MIDDLE_X=$((WIN_DESTOP_W/2 - CUR_WINDOW_GEOM_W/2))
